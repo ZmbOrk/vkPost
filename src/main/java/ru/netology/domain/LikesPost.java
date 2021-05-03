29f10cd19cba315cbd3eb780f89fb6c7ad1240ec
ZmbOrk <aleksei05030@gmail.com>
2021-05-03 19:37:18 +1000

    upd LikesPost

--- src/main/java/ru/netology/domain/LikesPost.java
+++ src/main/java/ru/netology/domain/LikesPost.java
@@ -3,19 +3,38 @@ package ru.netology.domain;
 public class LikesPost {
     private int count;
     private int userLikes;
+    private int canLike;
+    private int canPublish;
 
-    public int getCount () {
+    public int getCount() {
         return count;
     }
 
-    public void setCount(){
+    public void setCount(int count) {
         this.count = count;
     }
 
-    public int getUserLikes () {
+    public int getUserLikes() {
         return userLikes;
     }
-    public void setUserLikes () {
+
+    public void setUserLikes(int userLikes) {
         this.userLikes = userLikes;
+    }
+
+    public int getCanLike() {
+        return canLike;
+    }
+
+    public void setCanLike(int canLike) {
+        this.canLike = canLike;
+    }
+
+    public int getCanPublish() {
+        return canPublish;
+    }
+
+    public void setCanPublish(int canPublish) {
+        this.canPublish = canPublish;
     }
 }
